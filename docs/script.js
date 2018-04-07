5e268565938813aab8da6704c2d1cb95d5e0f41f
Daniel Catalán (soliddanii) <soliddanii3@gmail.com>
2018-04-07 18:45:59 +0200

    Actualizada pagina (Responsive)

--- docs/script.js
+++ docs/script.js
@@ -1,9 +1,24 @@
 $( document ).ready(function() {
 
-    // Fade in the elodie image
-    $("#elodie").animate({
-        left: "0px",
-        opacity: 1
-    }, 1500);
+    var viewportWidth = $("body").innerWidth();
+    var viewportHeight = $("body").innerHeight();
+
+    if(viewportWidth>700 && viewportHeight>500)
+    {
+
+        // Fade in the elodie image
+        $("#elodie").animate({
+            left: "0px",
+            opacity: 1
+        }, 1500, function() {
+            // Animation complete.
+            $(".wrapper").css("overflow", "auto");
+        });
+
+    }
+    else
+    {
+        $(".wrapper").css("overflow", "auto");
+    }
     
 });
